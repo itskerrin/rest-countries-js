b140fd0975a6ee73e9e0d89efd30271d6a45ffc8
kerrin <itskerrin@gmail.com>
2021-12-11 17:05:53 +0000

    added filtering by search and dropdown

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 const dropdownBtn = document.getElementById('dropdown-btn');
 const dropdownContent = document.getElementById('dropdown-content');
-const dropdownItem = document.querySelectorAll('.dropdown-item');
+const dropdownItem = dropdownContent.querySelectorAll('.dropdown-item');
 const countriesContainer = document.getElementById('countries-container');
+const searchbarInput = document.querySelector('input');
 
-// Filter by region
+// Drop down regions
 dropdownBtn.addEventListener('click', () => {
   dropdownContent.style.display === 'none' ||
   dropdownContent.style.display === ''
@@ -11,14 +12,7 @@ dropdownBtn.addEventListener('click', () => {
     : (dropdownContent.style.display = 'none');
 });
 
-// get region
-dropdownItem.forEach((region) => {
-  region.addEventListener('click', () => {
-    console.log(region.innerText);
-  });
-});
-
-// fetch countries and render each on screen
+// Fetch data and render cards for each country
 const fetchCountries = async () => {
   const res = await fetch('https://restcountries.com/v2/all');
   const countries = await res.json();
@@ -39,12 +33,42 @@ function displayCountries(countries) {
             <img src="${country.flag}" alt="${country.name}" />
         </div>
         <div class="card-content">
-            <h2>${country.name}</h2>
+            <h2 class="country-name">${country.name}</h2>
             <h2>Population: <span>${country.population.toLocaleString()}</span></h2>
-            <h2>Region: <span>${country.region}</span></h2>
+            <h2 class="country-region">Region: <span>${
+              country.region
+            }</span></h2>
             <h2>Capital: <span>${country.capital}</span></span></h2>
         </div>
         `;
     countriesContainer.appendChild(countryEl);
   });
 }
+
+// Search for country - search bar
+searchbarInput.addEventListener('input', (e) => {
+  const value = e.target.value;
+  const countryNames = document.querySelectorAll('.country-name');
+
+  countryNames.forEach((name) => {
+    name.innerText.toLowerCase().includes(value.toLowerCase())
+      ? (name.parentElement.parentElement.style.display = 'block')
+      : (name.parentElement.parentElement.style.display = 'none');
+  }); // change the visibility of 'countryEl' if it matches the input value
+});
+
+// Filter by region
+// add event listener to each dropdown item (regions)
+dropdownItem.forEach((regions) => {
+  regions.addEventListener('click', () => {
+    const value = regions.innerText;
+
+    const countryRegion = document.querySelectorAll('.country-region');
+    // check if clicked value matches h2 displaying regions
+    countryRegion.forEach((region) => {
+      region.innerText.includes(value) || value === 'All Regions'
+        ? (region.parentElement.parentElement.style.display = 'block')
+        : (region.parentElement.parentElement.style.display = 'none');
+    });
+  });
+});
